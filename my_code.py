a9d541c1244c27681f10ecae03a9b743ef43b95d
rishi ranjan <rishi.ranjan10@gmail.com>
2023-05-19 03:07:59 +0100

    too many changes

--- my_code.py
+++ my_code.py
@@ -1,5 +1,22 @@
-for e in range(10):
-    print("Change for the second commit")
-    print("Comiting to remote repository")
-    print("changed for the third commit")
+class Solution:
+    def the_sum_function(self,num_list,target):
+        test_list =[]
+        complement_dict = dict()
+        for e in range(len(num_list)):
+            test_list.append((e,target-num_list[e]))
+            print(test_list)
+
+        # for x,y in test_list:
+        #     if x in complement_dict:
+        #         return(test_list.index(x),)
+        
+
+
+
+s=Solution()
+print(s.the_sum_function([3,2,4],6))
+
+
+
+
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+
+        test_list =[]
+        complement_dict = dict()
+        for e in range(len(num_list)):
+            test_list.append((e,target-num_list[e]))
+        
